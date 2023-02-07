379439c2a41721739921755d61b56c2db9216853
DUB1401 <vlad.milosta@yandex.ru>
2023-02-07 17:24:50 +0300

    Add Apache 2.0 License

--- GUI/EvolvGen.h
+++ GUI/EvolvGen.h
@@ -11,8 +11,8 @@
 #define Endl std::endl
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 
 #include <windows.h>
 #include <iostream>
@@ -22,128 +22,129 @@
 #include <list>
 #include <map>
 
-//Перечисления и типы данных.
+// Перечисления и типы данных.
 namespace DUBLIB {
 
-	//Стандартные цвета, доступные для установки в консоль Windows.
+	// Стандартные цвета, доступные для установки в консоль Windows.
 	enum class CMD_Colors { clBlack = 0, clBlue, clGreen, clCyan, clRed, clMagenta, clBrown, clLightGray, clDarkGrey, clLightBlue, clLightGreen, clLightCyan, clLightRed, clLightMagenta, clYellow, clWhite };
 
-	//Типы строковых кодировок.
+	// Типы строковых кодировок.
 	enum class Encodings { ANSI = 0, UTF8, UTF16, UTF32 };
 
 }
 
-//Преобразование и анализ типов данных.
+// Преобразование и анализ типов данных.
 namespace DUBLIB {
 
-	//Преобразование целочисленного значения в строковое.
+	// Преобразование целочисленного значения в строковое.
 	std::string ConvertNumberToString(int Value);
 
-	//Преобразовывает std::string в std::wstring.
+	// Преобразовывает std::string в std::wstring.
 	std::wstring ToWstring(std::string Str);
 
-	//Преобразовывает std::wstring в std::string.
+	// Преобразовывает std::wstring в std::string.
 	std::string ToString(std::wstring Str, Encodings FromEncoding);
 
-
+	// Инвертирование значения переменной типа bool.
+	bool InvertBool(bool Value);
 }
 
-//Работа со строками.
+// Работа со строками.
 namespace DUBLIB {
 
-	//Удаляет указанное число символов с начала строки (ASCII).
+	// Удаляет указанное число символов с начала строки (ASCII).
 	std::string DeleteFirstCharacters(std::string Str, unsigned int Amount);
 
-	//Удаляет указанное число символов с начала строки (Unicode).
+	// Удаляет указанное число символов с начала строки (Unicode).
 	std::wstring DeleteFirstCharacters(std::wstring Str, unsigned int Amount);
 
-	//Удаляет указанное число символов с конца строки (ASCII).
+	// Удаляет указанное число символов с конца строки (ASCII).
 	std::string DeleteLastCharacters(std::string Str, unsigned int Amount);
 
-	//Удаляет указанное число символов с конца строки (Unicode).
+	// Удаляет указанное число символов с конца строки (Unicode).
 	std::wstring DeleteLastCharacters(std::wstring Str, unsigned int Amount);
 
-	//Удаляет пробелы и символы табуляции из начала и конца строки (ASCII).
+	// Удаляет пробелы и символы табуляции из начала и конца строки (ASCII).
 	std::string Trim(std::string Str);
 
-	//Удаляет пробелы и символы табуляции из начала и конца строки (Unicode).
+	// Удаляет пробелы и символы табуляции из начала и конца строки (Unicode).
 	std::wstring Trim(std::wstring Str);
 
-	//Обрезает строку до указанной длины (ASCII). 
+	// Обрезает строку до указанной длины (ASCII). 
 	std::string CutToLength(std::string Str, unsigned int Length);
 
-	//Обрезает строку до указанной длины (Unicode). 
+	// Обрезает строку до указанной длины (Unicode). 
 	std::wstring CutToLength(std::wstring Str, unsigned int Length);
 
-	//Проверяет посимвольно соответствие первой строки второй строке. Если первая строка длиннее второй, то усекает её до одинаковой длины (ASCII).
+	// Проверяет посимвольно соответствие первой строки второй строке. Если первая строка длиннее второй, то усекает её до одинаковой длины (ASCII).
 	bool CheckForSimilarity(std::string FirstStr, std::string SecondStr);
 
-	//Проверяет посимвольно соответствие первой строки второй строке. Если первая строка длиннее второй, то усекает её до одинаковой длины (Unicode).
+	// Проверяет посимвольно соответствие первой строки второй строке. Если первая строка длиннее второй, то усекает её до одинаковой длины (Unicode).
 	bool CheckForSimilarity(std::wstring FirstStr, std::wstring SecondStr);
 
-	//Разбивает строку по вхождению символа на подстроки (ASCII).
+	// Разбивает строку по вхождению символа на подстроки (ASCII).
 	std::vector<std::string> Split(std::string Str, char Marker);
 
-	//Разбивает строку по вхождению символа на подстроки (Unicode).
+	// Разбивает строку по вхождению символа на подстроки (Unicode).
 	std::vector<std::wstring> Split(std::wstring Str, wchar_t Marker);
 
 }
 
-//Математические функции.
+// Математические функции.
 namespace DUBLIB {
 
-	//Возвращает максимальные коэффициенты масштабирования изображения для пропорционального увеличения по оси Y.
+	// Возвращает максимальные коэффициенты масштабирования изображения для пропорционального увеличения по оси Y.
 	sf::Vector2f CalculateScaleY(sf::Vector2u FormSize, sf::Vector2u PictureSize, double DesiredRelatioToForm);
 
 }
 
-//Методы работы с текстовыми файлами Evolv.
+// Методы работы с текстовыми файлами Evolv.
 namespace DUBLIB {
 
-	//Возвращает значение первого найденного маркера в файле (ASCII).
+	// Возвращает значение первого найденного маркера в файле (ASCII).
 	std::string GetMarkeredStringFromFile(std::string File, std::string Marker);
 
-	//Возвращает значение первого найденного маркера в файле (UTF-16).
+	// Возвращает значение первого найденного маркера в файле (UTF-16).
 	std::wstring GetMarkeredStringFromFile(std::wstring File, std::wstring Marker);
 
-	//Возвращает целочисленное значение первого найденного маркера в файле (ASCII).
+	// Возвращает целочисленное значение первого найденного маркера в файле (ASCII).
 	int GetMarkeredIntFromFile(std::string File, std::string Marker);
 
-	//Возвращает целочисленное значение первого найденного маркера в файле (UTF-16).
+	// Возвращает целочисленное значение первого найденного маркера в файле (UTF-16).
 	int GetMarkeredIntFromFile(std::wstring File, std::wstring Marker);
 
-	//Возвращает значение первого найденного маркера в файле и форматирует его в bool (ASCII).
+	// Возвращает значение первого найденного маркера в файле и форматирует его в bool (ASCII).
 	bool GetMarkeredBoolFromFile(std::string File, std::string Marker);
 
-	//Возвращает значение первого найденного маркера в файле и форматирует его в bool (UTF-16).
+	// Возвращает значение первого найденного маркера в файле и форматирует его в bool (UTF-16).
 	bool GetMarkeredBoolFromFile(std::wstring File, std::wstring Marker);
 
-	//Возвращает вектор строк с соответствующим маркером (ASCII).
+	// Возвращает вектор строк с соответствующим маркером (ASCII).
 	std::vector<std::string> GetMarkeredStringsArrayFromFile(std::string File, std::string Marker);
 
-	//Возвращает вектор строк с соответствующим маркером (UTF-16).
+	// Возвращает вектор строк с соответствующим маркером (UTF-16).
 	std::vector<std::wstring> GetMarkeredStringsArrayFromFile(std::wstring File, std::wstring Marker);
 
 }
 
-//Debug-функции.
+// Debug-функции.
 namespace DUBLIB {
 
-	//Выводит в консоль содержимое вектора (std::string).
+	// Выводит в консоль содержимое вектора (std::string).
 	void PrintVector(std::vector<std::string> Value);
 
-	//Выводит в консоль содержимое вектора (std::wstring).
+	// Выводит в консоль содержимое вектора (std::wstring).
 	void PrintVector(std::vector<std::wstring> Value);
 
-	//Выводит в консоль содержимое вектора (unsigned int).
+	// Выводит в консоль содержимое вектора (unsigned int).
 	void PrintVector(std::vector<unsigned int> Value);
 
 }
 
-//Методы работы с Windows.
+// Методы работы с Windows.
 namespace DUBLIB {
 
-	//Задаёт цвет текста консоли.
+	// Задаёт цвет текста консоли.
 	void SetWindowsConsoleColor(CMD_Colors TextColor);
 
 }
